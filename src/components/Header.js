a6ff3c96152787cdecc908283efd8fdfd3074ea6
andron0712 <andriygryban07@gmail.com>
2026-06-01 14:18:30 +0300

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{ textAlign: 'center', marginBottom: '20px', borderBottom: '2px solid #3498db', paddingBottom: '10px' }}>
+      <h1 style={{ color: '#2c3e50', margin: 0 }}>Система керування задачами</h1>
+    </header>
+  );
+}
+
+export default Header;
